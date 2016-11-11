18b87fc5eb4b19d415d670131e399e0163eef61d
Duval Quentin <senmenty@gmail.com>
2016-11-11 14:08:10 +0100

    Create some primitives for the screen

--- src/cljs/visual/core.cljs
+++ src/cljs/visual/core.cljs
@@ -8,6 +8,7 @@
 
 (enable-console-print!)
 
+
 ;; -------------------------------------------------------------
 ;; Constants
 ;; -------------------------------------------------------------
@@ -15,13 +16,44 @@
 (defonce WIDTH 500)
 (defonce HEIGHT 500)
 
+
+;; -------------------------------------------------------------
+;; Create the main objects
+;; -------------------------------------------------------------
+
+(defrecord Point [x y])
+(defrecord Line [line-start line-end])
+
+(defn make-line [x0 y0 x1 y1]
+  (Line. (Point. x0 y0) (Point. x1 y1)))
+
+
+;; -------------------------------------------------------------
+;; Game state
+;; -------------------------------------------------------------
+
 (defonce game-state
-  (atom {:title "Draw shapes on the board"}))
+  (atom
+    {:title "Draw shapes on the board"
+     :lines [(make-line 100 100 100 100)]}
+    ))
 
 
 ;; -------------------------------------------------------------
-;; Draw loop
+;; Drawing entities
 ;; -------------------------------------------------------------
+
+(defn render-line
+  "Render a line on the screen"
+  [ctx line]
+  (-> ctx
+    (canvas/fill-style "red")
+    (canvas/fill-rect
+      {:x (:x (:line-start line))
+       :y (:y (:line-start line))
+       :w (:x (:line-end line))
+       :h (:y (:line-end line))})
+    ))
 
 (defn main-game-entity
   "Draw the main game entity in the canvas"
@@ -30,7 +62,8 @@
     @game-state
     (fn update [_] @game-state)
     (fn draw [ctx state]
-
+      (doseq [l (:lines state)]
+        (render-line ctx l))
       )))
 
 
